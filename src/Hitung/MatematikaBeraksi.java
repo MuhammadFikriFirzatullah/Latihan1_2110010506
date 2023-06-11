3da37ab2da38a97503b01fb5fc38c11ff8f24a5d
MuhammadFikriFirzatullah <fikrifti36@gmail.com>
2023-06-12 01:23:16 +0800

    Membuat class MatematikaBeraksi

--- src/Hitung/MatematikaBeraksi.java
+++ src/Hitung/MatematikaBeraksi.java
@@ -1,5 +1,15 @@
 package Hitung;
 
 public class MatematikaBeraksi {
+    public static void main(String[] args) {
+        // membuat objek
+        Matematika fikri = new Matematika (0,6);
+        
+        System.out.println("Hasil Penjumlahan: "+fikri.setPenjumlahan());
+        System.out.println("Hasil Pengurangan: "+fikri.setPengurangan());
+        System.out.println("Hasil Perkalian: "+fikri.setPerkalian());
+        System.out.println("Hasil Pembagian: "+fikri.setPembagian());
+        
+    }
     
 }
